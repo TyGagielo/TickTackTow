5a32834fc0cfda0e99dadf47e532c090977e84eb
Ty Gagielo <ty.gagielo@mrjaffesclass.com>
2023-02-22 12:47:06 -0800

    worj

---
+++ src/main/java/com/mycompany/ticktacktow/TickTackTow.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ticktacktow;
+
+/**
+ *
+ * @author student
+ */
+public class TickTackTow {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
